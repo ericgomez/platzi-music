bada851a4b567854246f6fc791827f557625513a
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-12 11:03:47 -0600

    Directivas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
-    p {{ 1 * 2}}
-    p {{ 'Hola' + 'Mundo' }}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase() }}
-    p {{ JSON.stringify(person) }}
-    p {{ true ? 'true' : 'false' }}
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="true") {{ 'algo mas' }}
+    p(v-else="showValue") {{ 'lo ultimo' }}
+
+    ul
+      li(v-for="i in items") {{ i }}
 </template>
 
 <script>
@@ -17,9 +17,10 @@ export default {
     return {
       msg: 'Hola Vue',
 
-      person: {
-        name: 'Eric'
-      }
+      showValue: false,
+      value: 'Algo',
+
+      items: [1, 2, 3, 4, 5]
     }
   }
 }
