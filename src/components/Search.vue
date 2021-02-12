33af773437dd1c4bb0f280d9b2f99b7a8f4a5087
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-12 17:40:11 -0600

    Modifiers

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -11,6 +11,7 @@
             type="text",
             placeholder="Buscar canciones",
             v-model="searchQuery"
+            @keyup.enter="search"
           )
           a.button.is-info.is-large(@click="search") Buscar
           a.button.is-danger.is-large &times;
