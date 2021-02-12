a95091876fee830d4d5665f8bfebe328a79266d6
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-12 16:41:14 -0600

    Comunicacion entre Componentes Genericos - Event Bus y Plugins -

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
-Vue.config.productionTip = false
+import EventBus from '@/plugins/event-bus'
+
+Vue.use(EventBus)
 
 new Vue({
   render: h => h(App)
